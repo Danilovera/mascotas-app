bf11f2b48acbf9b9d0d7e64ab00b2a7894844cdd
Danilo <danilovera02@gmail.com>
2022-04-15 00:12:12 -0500

    posible error

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const middleware = jsonServer.defaults()
 server.use(middleware)
 server.use(router)
 
-const port = process.env.port||4000
+const port = process.env.PORT || 4000
 server.listen(port,()=>{
     console.log("esta shit esta corriendo")
 })
+
