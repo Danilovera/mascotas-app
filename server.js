3613ba4ed53270dfabf09ec8434c23afdbc23502
Danilo <danilovera02@gmail.com>
2022-04-15 00:01:15 -0500

    funciona Heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server")
+const server = jsonServer.create()
+const router = jsonServer. router('data.json')
+const middleware = jsonServer.defaults()
+server.use(middleware)
+server.use(router)
+
+const port = process.env.port||4000
+server.listen(port,()=>{
+    console.log("esta shit esta corriendo")
+})
